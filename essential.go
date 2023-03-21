0adddfa1311f3189753bd933246745af67169dec
barry <barry.z97822@gmail.com>
2023-03-21 16:55:09 -0400

    added execute script

--- essential.go
+++ essential.go
@@ -1,9 +1,19 @@
 package mysyscall
 
 import (
+	"fmt"
+	"os/exec"
 	"syscall"
 )
 
 func MySetGid(id int) {
 	syscall.Setgid(id)
+}
+
+func RunScript(cmdStr string) {
+	out, err := exec.Command(cmdStr).CombinedOutput()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", out)
 }
