197b4aa525c798c09132081043c6bf4983ee461e
barry <barry.z97822@gmail.com>
2023-03-23 21:28:40 -0400

    added comments

--- essential.go
+++ essential.go
@@ -5,10 +5,12 @@ import (
 	"syscall"
 )
 
+// wrapper function for syscall's wrapper function
 func MySetGid(id int) {
 	syscall.Setgid(id)
 }
 
+// Wrapper function for os/exec's shell command execution function
 func RunScript(cmdStr string) ([]byte, error) {
 	out, err := exec.Command(cmdStr).CombinedOutput()
 	return out, err
